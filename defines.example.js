0d97806f0f1559e599efdab16c618b46bafeeffc
Alex Studer <astuder@notetoscreen.com>
2018-03-28 23:55:53 -0400

    fix example api host to use https

--- defines.example.js
+++ defines.example.js
@@ -1,4 +1,4 @@
 module.exports = {
-	MHS_API_HOST: JSON.stringify("http://api-v2.myhomework.space/"),
+	MHS_API_HOST: JSON.stringify("https://api-v2.myhomework.space/"),
 	MHS_CLIENT_ID: JSON.stringify("(set this to your client id)")
 };
