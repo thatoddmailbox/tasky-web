4dfeda7ad6715f725b2c4d7b8adfdc4b5af34e26
Alex Studer <astuder@notetoscreen.com>
2018-06-27 15:44:30 +0200

    add support for renaming lists

--- app/manager/ListManagerList.jsx
+++ app/manager/ListManagerList.jsx
@@ -35,11 +35,29 @@ export default class ListManagerList extends Component {
 		});
 	}
 
+	rename(list) {
+		var that = this;
+		var newListName = prompt("Enter a new name for the list", list.listName);
+		if (newListName) {
+			var newName = "To-do (" + (list.archived ? "archived, " : "") + newListName + ")";
+			mhs.post(that.props.token, "classes/edit", {
+				id: list.id,
+				color: "40ccff",
+				name: newName
+			}, function(data) {
+				that.props.getLists();
+			});
+		}
+	}
+
 	render(props, state) {
 		var list = props.list;
 		return <div class={`listManagerList row ${list.archived ? "archived" : ""}`}>
 			<div class="col listManagerListName">{list.listName}</div>
 			<div class="col listManagerActions">
+				<button class="btn btn-sm btn-secondary" onClick={this.rename.bind(this, list)}>
+					<i class="fa fa-pencil" /> rename
+				</button>
 				<button class="btn btn-sm btn-secondary" onClick={this.toggleArchived.bind(this, list)} disabled={!!state.loading}>
 					{state.loading ? "loading..." : <span><i class="fa fa-archive" /> {list.archived ? "unarchive" : "archive"}</span>}
 				</button>
